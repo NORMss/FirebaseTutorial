7784e54a1f8d243ab71790d093ed442df95b7cfb
NORMss <serega.b301@gmail.com>
2024-08-08 20:49:34 +0700

    feat: added reading data from the server and a button for adding
    Closes #2

--- app/src/main/java/com/norm/myfirebasetutorial/MainActivity.kt
+++ app/src/main/java/com/norm/myfirebasetutorial/MainActivity.kt
@@ -6,16 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.norm.myfirebasetutorial.data.Technology
 import com.norm.myfirebasetutorial.ui.theme.MyFirebaseTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,23 +38,84 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyFirebaseTutorialTheme {
-                val fs = Firebase.firestore
-                val result = fs
-                    .collection("technologies")
-                    .document("id")
-                    .set(
-                        mapOf("title" to "Navigation")
-                    )
-                Column(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Text(
-                        text = result.exception?.message ?: "Unavailable"
+                ) { padding ->
+                    MainScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                top = padding.calculateTopPadding(),
+                                bottom = padding.calculateBottomPadding(),
+                            ),
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    val fs = Firebase.firestore
+    val list = remember {
+        mutableStateOf(emptyList<Technology>())
+    }
+
+    val listener = fs.collection("technologies").addSnapshotListener { snapShot, exeption ->
+        list.value = snapShot?.toObjects(Technology::class.java) ?: emptyList()
+    }
+//    listener.remove()
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.8f),
+        ) {
+            items(list.value) { technology ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth()
+                            .padding(16.dp),
+                        text = technology.name,
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(),
+            onClick = {
+                fs
+                    .collection("technologies")
+                    .document()
+                    .set(
+                        Technology(
+                            name = "Room",
+                            description = "The Room persistence library provides an abstraction layer over SQLite to allow for more robust database access while harnessing the full power of SQLite.",
+                            platform = "Android/iOS",
+                            version = "2.6.1",
+                            mavenUrl = "androidx.room:room-runtime",
+                            logoUrl = "https://developer.android.com/static/images/training/data-storage/room_architecture.png",
+                        )
+
+                    )
+            }
+        ) {
+            Text(
+                text = "Add Technology"
+            )
         }
     }
 }
---
+++ app/src/main/java/com/norm/myfirebasetutorial/data/Technology.kt
@@ -0,0 +1,10 @@
+package com.norm.myfirebasetutorial.data
+
+data class Technology(
+    val name: String = "",
+    val description: String = "",
+    val platform: String = "",
+    val version: String = "",
+    val mavenUrl: String = "",
+    val logoUrl: String = "",
+)
