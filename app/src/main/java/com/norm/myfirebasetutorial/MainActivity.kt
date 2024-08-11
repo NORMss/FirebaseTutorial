91842354b173907776b87ed7aa15431b9f379809
NORMss <serega.b301@gmail.com>
2024-08-11 19:43:11 +0700

    feat: added login, logout and deletion
    Closes #6

--- app/src/main/java/com/norm/myfirebasetutorial/MainActivity.kt
+++ app/src/main/java/com/norm/myfirebasetutorial/MainActivity.kt
@@ -5,11 +5,11 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -29,18 +28,22 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -48,8 +51,12 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import com.norm.myfirebasetutorial.data.Technology
 import com.norm.myfirebasetutorial.presentation.AuthScreen
+import com.norm.myfirebasetutorial.presentation.deleteAccount
+import com.norm.myfirebasetutorial.presentation.singIn
+import com.norm.myfirebasetutorial.presentation.singOut
 import com.norm.myfirebasetutorial.presentation.singUp
 import com.norm.myfirebasetutorial.ui.theme.MyFirebaseTutorialTheme
+import kotlinx.coroutines.launch
 import java.io.ByteArrayOutputStream
 
 class MainActivity : ComponentActivity() {
@@ -79,9 +86,32 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
+            val scope = rememberCoroutineScope()
+            val snackbarHostState = remember {
+                SnackbarHostState()
+            }
+            var snackbarMessage by remember {
+                mutableStateOf("")
+            }
+
+            LaunchedEffect(snackbarMessage) {
+                scope.launch {
+                    snackbarHostState.showSnackbar(snackbarMessage)
+                }
+            }
+
+            Log.d("MyLog", "Current user's mail: ${fa.currentUser?.email ?: "Unknown"}")
+            if (fa.currentUser?.email != null) {
+                snackbarMessage = "Current user's mail: ${
+                    fa.currentUser?.email
+                }"
+            }
             MyFirebaseTutorialTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
+                    snackbarHost = {
+                        SnackbarHost(hostState = snackbarHostState)
+                    }
                 ) { padding ->
 //                    MainScreen(
 //                        modifier = Modifier
@@ -105,8 +135,27 @@ class MainActivity : ComponentActivity() {
                                 top = padding.calculateTopPadding(),
                                 bottom = padding.calculateBottomPadding(),
                             ),
-                        onAuth = { email, password ->
-                            singUp(
+                        onSignUp = { email, password ->
+                            snackbarMessage = singUp(
+                                auth = fa,
+                                email = email,
+                                password = password,
+                            )
+                        },
+                        onSignIn = { email, password ->
+                            snackbarMessage = singIn(
+                                auth = fa,
+                                email = email,
+                                password = password,
+                            )
+                        },
+                        onSignOut = {
+                            snackbarMessage = singOut(
+                                auth = fa,
+                            )
+                        },
+                        onDeleteAccount = { email, password ->
+                            snackbarMessage = deleteAccount(
                                 auth = fa,
                                 email = email,
                                 password = password,
--- app/src/main/java/com/norm/myfirebasetutorial/presentation/AuthScreen.kt
+++ app/src/main/java/com/norm/myfirebasetutorial/presentation/AuthScreen.kt
@@ -1,22 +1,21 @@
 package com.norm.myfirebasetutorial.presentation
 
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,15 +25,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import com.google.firebase.auth.EmailAuthCredential
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun AuthScreen(
     modifier: Modifier = Modifier,
-    onAuth: (login: String, password: String) -> Unit,
+    onSignUp: (login: String, password: String) -> Unit,
+    onSignIn: (login: String, password: String) -> Unit,
+    onSignOut: () -> Unit,
+    onDeleteAccount: (login: String, password: String) -> Unit,
 ) {
     val context = LocalContext.current
     Column(
@@ -99,28 +102,126 @@ fun AuthScreen(
             modifier = Modifier
                 .height(16.dp)
         )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Button(
+                onClick = {
+                    onSignIn(
+                        login,
+                        password,
+                    )
+                }
+            ) {
+                Text(
+                    text = "Sign In"
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .width(16.dp)
+            )
+            Button(
+                onClick = {
+                    onSignUp(
+                        login,
+                        password,
+                    )
+                }
+            ) {
+                Text(
+                    text = "Sign Up"
+                )
+            }
+        }
+        Spacer(
+            modifier = Modifier
+                .height(16.dp)
+        )
         Button(
             onClick = {
-                onAuth(
+                onSignOut()
+            }
+        ) {
+            Text(
+                text = "Sign Out"
+            )
+        }
+        Spacer(
+            modifier = Modifier
+                .height(16.dp)
+        )
+        Button(
+            onClick = {
+                onDeleteAccount(
                     login,
                     password,
                 )
             }
         ) {
             Text(
-                text = "Sign In"
+                text = "Delete account"
             )
         }
     }
 }
 
-fun singUp(auth: FirebaseAuth, email: String, password: String) {
+fun singUp(auth: FirebaseAuth, email: String, password: String): String {
+    var status = ""
     auth.createUserWithEmailAndPassword(email, password)
         .addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 Log.d("MyLog", "Sign Up successful")
+                status = "Sign Up successful"
             } else {
                 Log.d("MyLog", "Sign Up failure")
+                status = "Sign Up failure"
             }
         }
+    return status
+}
+
+fun singIn(auth: FirebaseAuth, email: String, password: String): String {
+    var status = ""
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("MyLog", "Sign In successful")
+                status = "Sign In successful"
+            } else {
+                Log.d("MyLog", "Sign Up failure")
+                status = "Sign In failure"
+            }
+        }
+    return status
+}
+
+fun singOut(auth: FirebaseAuth): String {
+    auth.signOut()
+    return "signOut"
+}
+
+fun deleteAccount(auth: FirebaseAuth, email: String, password: String): String {
+    var status = ""
+    val credential = EmailAuthProvider.getCredential(
+        email,
+        password,
+    )
+    auth.currentUser?.reauthenticate(credential)?.addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            auth.currentUser?.delete()?.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("MyLog", "Delete successful")
+                    status = "Delete successful"
+                } else {
+                    Log.d("MyLog", "Delete failure")
+                    status = "Delete failure"
+                }
+            }
+        } else {
+            Log.d("MyLog", "Reauthenticate failure")
+            status = "Reauthenticate failure"
+        }
+    }
+    return status
 }
